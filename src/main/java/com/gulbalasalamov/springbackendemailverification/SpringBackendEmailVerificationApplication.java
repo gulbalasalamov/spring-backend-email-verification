ff2a5117dead991df3ecd387da86fd988c306366
gulbalasalamov <g.salamov@gmail.com>
2021-09-13 11:13:39 +0300

    Initial commit

---
+++ src/main/java/com/gulbalasalamov/springbackendemailverification/SpringBackendEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.springbackendemailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendEmailVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gulbalasalamov/springbackendemailverification/SpringBackendEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.springbackendemailverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendEmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
